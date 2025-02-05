1912757788f0412d6c175d3f15bee6ae78433a17
srxrt <str9703@gmail.com>
2025-02-05 16:42:50 +0900

    feat: develop notify on comment logic

--- apps/nestar-api/src/components/comment/comment.module.ts
+++ apps/nestar-api/src/components/comment/comment.module.ts
@@ -1,21 +1,23 @@
-import { Module } from '@nestjs/common';
-import { CommentResolver } from './comment.resolver';
-import { CommentService } from './comment.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from '../auth/auth.module';
-import { MemberModule } from '../member/member.module';
-import CommentSchema from '../../schemas/Comment.model';
-import { PropertyModule } from '../property/property.module';
-import { BoardArticleModule } from '../board-article/board-article.module';
+import { Module } from "@nestjs/common";
+import { CommentResolver } from "./comment.resolver";
+import { CommentService } from "./comment.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuthModule } from "../auth/auth.module";
+import { MemberModule } from "../member/member.module";
+import CommentSchema from "../../schemas/Comment.model";
+import { PropertyModule } from "../property/property.module";
+import { BoardArticleModule } from "../board-article/board-article.module";
+import { NotificationModule } from "../notification/notification.module";
 
 @Module({
-	imports: [
-		MongooseModule.forFeature([{ name: 'Comment', schema: CommentSchema }]),
-		AuthModule,
-		PropertyModule,
-		MemberModule,
-		BoardArticleModule,
-	],
-	providers: [CommentResolver, CommentService],
+  imports: [
+    MongooseModule.forFeature([{ name: "Comment", schema: CommentSchema }]),
+    AuthModule,
+    PropertyModule,
+    MemberModule,
+    BoardArticleModule,
+    NotificationModule,
+  ],
+  providers: [CommentResolver, CommentService],
 })
-export class CommentModule {}
+export class CommentModule { }
--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -1,88 +1,138 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model, ObjectId } from 'mongoose';
-import { Comment, Comments } from '../../libs/dto/comment/comment';
-import { MemberService } from '../member/member.service';
-import { PropertyService } from '../property/property.service';
-import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
-import { BoardArticleService } from '../board-article/board-article.service';
-import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
-import { Direction, Message } from '../../libs/enums/common.enum';
-import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
-import { CommentUpdate } from '../../libs/dto/comment/comment.update';
-import { T } from '../../libs/types/common';
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, ObjectId } from "mongoose";
+import { Comment, Comments } from "../../libs/dto/comment/comment";
+import { MemberService } from "../member/member.service";
+import { PropertyService } from "../property/property.service";
+import {
+  CommentInput,
+  CommentsInquiry,
+} from "../../libs/dto/comment/comment.input";
+import { BoardArticleService } from "../board-article/board-article.service";
+import { CommentGroup, CommentStatus } from "../../libs/enums/comment.enum";
+import { Direction, Message } from "../../libs/enums/common.enum";
+import { lookupMember, shapeIntoMongoObjectId } from "../../libs/config";
+import { CommentUpdate } from "../../libs/dto/comment/comment.update";
+import { T } from "../../libs/types/common";
+import { NotificationInput } from "../../libs/dto/notification/notification.input";
+import {
+  NotificationGroup,
+  NotificationType,
+} from "../../libs/enums/notification.enum";
+import { NotificationService } from "../notification/notification.service";
 
 @Injectable()
 export class CommentService {
   constructor(
-    @InjectModel('Comment') private readonly commentModel: Model<Comment>,
+    @InjectModel("Comment") private readonly commentModel: Model<Comment>,
     private memberService: MemberService,
     private propertyService: PropertyService,
     private boardArticleService: BoardArticleService,
+    private notificationService: NotificationService,
   ) { }
 
-  public async createComment(memberId: ObjectId, input: CommentInput): Promise<Comment> {
+  public async createComment(
+    memberId: ObjectId,
+    input: CommentInput,
+  ): Promise<Comment> {
     input.memberId = memberId;
     input.commentRefId = shapeIntoMongoObjectId(input.commentRefId);
     let result = null;
     try {
       result = await this.commentModel.create(input);
     } catch (err) {
-      console.log('Comment Service Error: createComment', err);
+      console.log("Comment Service Error: createComment", err);
       throw new InternalServerErrorException(Message.CREATE_FAILED);
     }
+
+    const notifInput: NotificationInput = {
+      notificationGroup: null,
+      notificationType: NotificationType.COMMENT,
+      notificationTitle: "",
+      notificationDesc: "",
+      authorId: memberId,
+      receiverId: null,
+    };
+
+    let target: any;
     switch (input.commentGroup) {
       case CommentGroup.PROPERTY:
-        await this.propertyService.propertyStatsEditor({
+        target = await this.propertyService.propertyStatsEditor({
           _id: input.commentRefId,
-          targetKey: 'propertyComments',
+          targetKey: "propertyComments",
           modifier: 1,
         });
+        notifInput.notificationGroup = NotificationGroup.PROPERTY;
+        notifInput.notificationTitle = "Property Comment!";
+        notifInput.notificationDesc = "Someone commented on your property!";
+        notifInput.receiverId = target.memberId;
+        notifInput.propertyId = input.commentRefId;
+
         break;
       case CommentGroup.ARTICLE:
-        await this.boardArticleService.boardArticleStatsEditor({
+        target = await this.boardArticleService.boardArticleStatsEditor({
           _id: input.commentRefId,
-          targetKey: 'articleComments',
+          targetKey: "articleComments",
           modifier: 1,
         });
+        notifInput.notificationGroup = NotificationGroup.ARTICLE;
+        notifInput.notificationTitle = "Article Comment!";
+        notifInput.notificationDesc = "Someone commented on your article!";
+        notifInput.receiverId = target.memberId;
+        notifInput.articleId = input.commentRefId;
         break;
       case CommentGroup.MEMBER:
         await this.memberService.memberStatsModifier({
           _id: input.commentRefId,
-          targetKey: 'memberComments',
+          targetKey: "memberComments",
           modifier: 1,
         });
+        notifInput.notificationGroup = NotificationGroup.MEMBER;
+        notifInput.notificationTitle = "Profile Comment!";
+        notifInput.notificationDesc = "Someone commented on your profile!";
+        notifInput.receiverId = input.commentRefId;
         break;
     }
+    await this.notificationService.createNotification(notifInput);
+
     if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
     return result;
   }
 
-  public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
-    const search: T = { _id: input._id, memberId: memberId, commentStatus: CommentStatus.ACTIVE };
+  public async updateComment(
+    memberId: ObjectId,
+    input: CommentUpdate,
+  ): Promise<Comment> {
+    const search: T = {
+      _id: input._id,
+      memberId: memberId,
+      commentStatus: CommentStatus.ACTIVE,
+    };
     const { commentStatus } = input;
-    const result = await this.commentModel.findOneAndUpdate(search, input, { new: true }).exec();
+    const result = await this.commentModel
+      .findOneAndUpdate(search, input, { new: true })
+      .exec();
     if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
     if (commentStatus === CommentStatus.DELETE) {
       switch (result.commentGroup) {
         case CommentGroup.PROPERTY:
           await this.propertyService.propertyStatsEditor({
             _id: result.commentRefId,
-            targetKey: 'propertyComments',
+            targetKey: "propertyComments",
             modifier: -1,
           });
           break;
         case CommentGroup.ARTICLE:
           await this.boardArticleService.boardArticleStatsEditor({
             _id: result.commentRefId,
-            targetKey: 'articleComments',
+            targetKey: "articleComments",
             modifier: -1,
           });
           break;
         case CommentGroup.MEMBER:
           await this.memberService.memberStatsModifier({
             _id: result.commentRefId,
-            targetKey: 'memberComments',
+            targetKey: "memberComments",
             modifier: -1,
           });
           break;
@@ -91,10 +141,18 @@ export class CommentService {
     return result;
   }
 
-  public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+  public async getComments(
+    memberId: ObjectId,
+    input: CommentsInquiry,
+  ): Promise<Comments> {
     const { commentRefId } = input.search;
-    const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
-    const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+    const match: T = {
+      commentRefId: commentRefId,
+      commentStatus: CommentStatus.ACTIVE,
+    };
+    const sort: T = {
+      [input?.sort ?? "createdAt"]: input?.direction ?? Direction.DESC,
+    };
     const result: Comments[] = await this.commentModel
       .aggregate([
         { $match: match },
@@ -105,14 +163,15 @@ export class CommentService {
               { $skip: (input.page - 1) * input.limit },
               { $limit: input.limit },
               lookupMember,
-              { $unwind: '$memberData' },
+              { $unwind: "$memberData" },
             ],
-            metaCounter: [{ $count: 'total' }],
+            metaCounter: [{ $count: "total" }],
           },
         },
       ])
       .exec();
-    if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+    if (!result.length)
+      throw new InternalServerErrorException(Message.NO_DATA_FOUND);
     return result[0];
   }
 
