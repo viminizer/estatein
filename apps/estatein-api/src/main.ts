ae79de4697376e2d7fb3238ad766943aa07f7b37
srxrt <str9703@gmail.com>
2025-02-14 03:02:11 +0900

    feat: create commit.sh script

--- apps/estatein-api/src/main.ts
+++ apps/estatein-api/src/main.ts
@@ -1,19 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
-import { graphqlUploadExpress } from 'graphql-upload';
-import * as express from 'express';
-import { WsAdapter } from '@nestjs/platform-ws';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { LoggingInterceptor } from "./libs/interceptor/Logging.interceptor";
+import { graphqlUploadExpress } from "graphql-upload";
+import * as express from "express";
+import { WsAdapter } from "@nestjs/platform-ws";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.useGlobalPipes(new ValidationPipe());
-	app.useGlobalInterceptors(new LoggingInterceptor());
-	app.enableCors({ origin: true, credentials: true });
-	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
-	app.use('/uploads', express.static('./uploads'));
-  app.useWebSocketAdapter(new WsAdapter(app))
-	await app.listen(process.env.PORT_API ?? 3007);
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.enableCors({ origin: true, credentials: true });
+  app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
+  app.use("/uploads", express.static("./uploads"));
+  app.useWebSocketAdapter(new WsAdapter(app));
+  await app.listen(process.env.PORT_API ?? 3007);
 }
 bootstrap();
