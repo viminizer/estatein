8402e0717cec8a0ea9df6dca3c90698d2b2aeb99
srxrt <str9703@gmail.com>
2025-04-12 04:57:09 +0900

    fix: modify notification model

--- apps/estatein-api/src/schemas/Notification.model.ts
+++ apps/estatein-api/src/schemas/Notification.model.ts
@@ -1,58 +1,62 @@
-import { Schema } from 'mongoose';
-import { NotificationGroup, NotificationStatus, NotificationType } from '../libs/enums/notification.enum';
+import { Schema } from "mongoose";
+import {
+  NotificationGroup,
+  NotificationStatus,
+  NotificationType,
+} from "../libs/enums/notification.enum";
 
 const NotificationSchema = new Schema(
-	{
-		notificationType: {
-			type: String,
-			enum: NotificationType,
-			required: true,
-		},
+  {
+    notificationType: {
+      type: String,
+      enum: NotificationType,
+      required: true,
+    },
 
-		notificationStatus: {
-			type: String,
-			enum: NotificationStatus,
-			default: NotificationStatus.WAIT,
-		},
+    notificationStatus: {
+      type: String,
+      enum: NotificationStatus,
+      default: NotificationStatus.WAIT,
+    },
 
-		notificationGroup: {
-			type: String,
-			enum: NotificationGroup,
-			required: true,
-		},
+    notificationGroup: {
+      type: String,
+      enum: NotificationGroup,
+      required: true,
+    },
 
-		notificationTitle: {
-			type: String,
-			required: true,
-		},
+    notificationTitle: {
+      type: String,
+      required: true,
+    },
 
-		notificationDesc: {
-			type: String,
-		},
+    notificationDesc: {
+      type: String,
+    },
 
-		authorId: {
-			type: Schema.Types.ObjectId,
-			required: true,
-			ref: 'Member',
-		},
+    authorId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Member",
+    },
 
-		receiverId: {
-			type: Schema.Types.ObjectId,
-			required: true,
-			ref: 'Member',
-		},
+    receiverId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Member",
+    },
 
-		propertyId: {
-			type: Schema.Types.ObjectId,
-			ref: 'Property',
-		},
+    propertyId: {
+      type: Schema.Types.ObjectId,
+      ref: "Property",
+    },
 
-		articleId: {
-			type: Schema.Types.ObjectId,
-			ref: 'BoardArticle',
-		},
-	},
-	{ timestamps: true, collection: 'notifications' },
+    articleId: {
+      type: Schema.Types.ObjectId,
+      ref: "BoardArticle",
+    },
+  },
+  { timestamps: true, collection: "notifications" },
 );
 
 export default NotificationSchema;
